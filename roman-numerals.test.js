594782a75e7ac7ba362fce5cdc4599eed1a4e034
javigomez <javier.gomez.corio@gmail.com>
2018-11-12 09:03:38 +0100

    feat: given 1 returns I

--- roman-numerals.test.js
+++ roman-numerals.test.js
@@ -1,4 +1,7 @@
 // http://kata-log.rocks/roman-numerals-kata
+function convert(arabicNumber) {
+    if (arabicNumber == 1) return 'I'
+}
 
 test('given 1 returns I', () => {
     expect(convert(1)).toBe('I')
