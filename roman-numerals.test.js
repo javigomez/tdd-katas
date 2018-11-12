0b317b7148551fcfbbb5e8a27b0f44364a754c5d
javigomez <javier.gomez.corio@gmail.com>
2018-11-12 09:07:43 +0100

    test: given 2 returns II

--- roman-numerals.test.js
+++ roman-numerals.test.js
@@ -1,8 +1,18 @@
 // http://kata-log.rocks/roman-numerals-kata
 function convert(arabicNumber) {
-    if (arabicNumber == 1) return 'I'
+    romanNumber = ''
+    while(arabicNumber > 0) {
+        romanNumber += 'I'
+        arabicNumber--
+    }
+    
+    return romanNumber
 }
 
 test('given 1 returns I', () => {
     expect(convert(1)).toBe('I')
+})
+
+test('given 2 returns II', () => {
+    expect(convert(2)).toBe('II')
 })
